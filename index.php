8a69698e481eb4dc8756ba60c7549647e3241686
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-04-21 21:53:13 -0700

    video 15

--- index.php
+++ index.php
@@ -9,6 +9,22 @@
 		<div class="task-list">
 			<ul>
 				<?php require("includes/connect.php");
+				$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+					$query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";
+					if ($result = $mysqli->query($query)) {
+
+					 	$numrows = $result->num_rows;
+					 	if ($numrows>0) {
+					 		while ($row = $result->fetch_assoc()) {
+					 			$task_id = $row['id'];
+					 			$task_name = $row['task'];
+
+					 			echo '<li>
+					 			<span>' .$task_name. '</span> 
+					 			<img id="'.$task_id.'" class = "delete-button" width="10px" src="images/close.svg"/> </li>' ;
+					 		}
+					 	}
+					 } 
 				?>
 			</ul>
 		</div>
