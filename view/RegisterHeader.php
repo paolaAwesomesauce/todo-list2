d60800e35baa8c83ce9a86d3819d4c2deda21d08
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-04-28 10:44:22 -0700

    css

--- view/RegisterHeader.php
+++ view/RegisterHeader.php
@@ -4,7 +4,7 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/register.css"> 
 	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-	<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+	<!-- <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">  -->
 	<title></title>
 </head>
 <body>
--- view/header.php
+++ view/header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <!-- <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">  -->
+  <!-- <meta name="viewport" content="width=device-width" /> -->
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/index.css"> 
 	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-	<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 	<title></title>
 </head>
 <body>
